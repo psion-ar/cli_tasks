03e7aa5fb33221138c66fc4a177c38c1dba437b6
psion-ar <psion@tuta.io>
2023-03-16 08:00:29 +0100

    added method to format dates to a better human readible format

--- src/Command.php
+++ src/Command.php
@@ -2,7 +2,9 @@
 
 namespace Ar\CliTasks;
 
+use Carbon\Carbon;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class Command extends SymfonyCommand
 {
@@ -15,14 +17,32 @@ class Command extends SymfonyCommand
         parent::__construct();
     }
 
-    public function showTasks($io)
+    protected function showTasks(SymfonyStyle $io): void
     {
         if (!$tasks = $this->database->fetchAll('tasks')) {
             $io->info('You have no pending tasks right now.');
             return;
         }
 
+        $formatedTasks = $this->formatTasks($tasks);
+
         $io->title('Your pending tasks:');
-        $io->table(['id', 'Created At', 'Description', 'Due Date'], $tasks);
+        $io->table(['id', 'Created At', 'Description', 'Due Date'], $formatedTasks);
+    }
+
+    protected function formatTasks(array $tasks): array
+    {
+        $formatedTasks = [];
+
+        foreach ($tasks as $task) {
+            $formatedTasks[] = [
+                'id' => $task['id'],
+                'created_at' => Carbon::createFromDate($task['created_at'])->diffForHumans(),
+                'description' => strtoupper($task['description']),
+                'due_date' => Carbon::createFromDate($task['due_date'])->diffForHumans(),
+            ];
+        }
+
+        return $formatedTasks;
     }
 }
