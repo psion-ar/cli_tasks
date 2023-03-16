a99d1fd20ff0801816b84c3441f2cf23c37359c4
psion-ar <psion@tuta.io>
2023-03-16 13:28:40 +0100

    added a Command to add new tasks

--- src/AddCommand.php
+++ src/AddCommand.php
@@ -2,6 +2,7 @@
 
 namespace Ar\CliTasks;
 
+use Carbon\Carbon;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -18,6 +19,57 @@ class AddCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
+        $task = $this->getTaskDescription($io);
+        $dueDate = $this->getDueDate($io);
+
+        $io->listing([
+            $task,
+            $dueDate ?? 'no due date given',
+        ]);
+
+        $this->database->query('INSERT INTO tasks(task, due_date) VALUES(:task, :due_date)', [
+            'task' => $task,
+            'due_date' => $dueDate,
+        ]);
+
+        $this->showTasks($io);
+
         return Command::SUCCESS;
+    }
+
+    private function getTaskDescription(SymfonyStyle $io): string
+    {
+        $newTask = '';
+
+        do {
+            $newTask = $io->ask('Add a new task');
+            (!$newTask)
+            ? $io->warning('Come on ... at least type something ...')
+            : $io->success("Task: {$newTask}");
+        } while (!$newTask);
+
+        return $newTask;
+    }
+
+    private function getDueDate(SymfonyStyle $io): mixed
+    {
+        if ($io->confirm('Do you want to add a due date?', false)) {
+            $dueDate = $io->choice('When is the task due?', [
+                'tomorrow',
+                'in two days',
+                'in three days',
+                'in one week',
+            ]);
+        }
+
+        $result = match($dueDate ?? 'null') {
+            'null' => null,
+            'tomorrow' => Carbon::now()->addDay(),
+            'in two days' => Carbon::now()->addDays(2),
+            'in three days' => Carbon::now()->addDays(3),
+            'in one week' => Carbon::now()->addWeek(),
+        };
+
+        return $result;
     }
 }
